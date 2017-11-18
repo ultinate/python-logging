87da9fdbf125545ff4abff2816bcc24b71beb9c7
Nathanael Wettstein <nathanael.wettstein@gmail.com>
2017-11-18 23:13:22 +0100

    Add setting disable_existing_loggers. Cleanup.

--- demo.py
+++ demo.py
@@ -2,12 +2,13 @@ import yaml
 import logging
 
 import bar
-import foo
+from foo import Foo
 
 log = logging.getLogger(__name__)
 
 
 def setup_logging():
+    import logging.config
     with open('log_config.yaml') as f:
         conf = yaml.load(f)
         logging.config.dictConfig(conf)
@@ -16,12 +17,12 @@ def setup_logging():
 def main():
     log.info('Entering main ...')
     log.debug('This is a demo for logging.')
-    f = foo.Foo()
+    f = Foo()
     f.do_foo(42)
     bar.bar()
     log.info('Leaving main ...')
 
 
 if __name__ == '__main__':
+    setup_logging()
     main()
-
--- foo.py
+++ foo.py
@@ -7,8 +7,7 @@ class Foo:
     def __init__(self):
         pass
 
-    def do_foo(self, error_code=''):
+    def do_foo(self, error_code):
         logger.info('Initializing Foo ...')
         logger.debug('Arguments: error={}'.format(error_code))
-        if error_code:
-            logger.error('Error "{}"'.format(error_code))
+        logger.error('Error "{}"'.format(error_code))
